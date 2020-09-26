126e4fc2eb62b07a84fdeaa3f324e15f2c70813d
lmilam <31598791+lmilam@users.noreply.github.com>
2020-09-25 21:23:34 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -2,69 +2,69 @@
 #include "shell.h"
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
 #include <unistd.h>
-
+#include <string.h>
+#include <stdlib.h>
+int execvp(const char *file, char *const argv[]);
 
 void execute(char* const args[])
 {
-  int i;
-  printf("*** Entered:");
-  for (i = 0; args[i] != NULL; i++)
-    printf(" %s", args[i]);
-  printf(" (%d words)\n", i);
-
-  int status = exec args;
-  if (status == 1)
-  { /* error occurred */
-    fprintf(stderr, "Command not found\n");
-    fprintf("Command exited with %d\n",status);              // status code other   
-    return 1;
-  }
-  if else(status == 2)
-  {
-    char delim[] = " ";
-    char *ptr = strtok(args, delim);
-    ptr = strtok(NULL, delim);
-    fprintf("%d: cannot access '%d' : No such file or directory exists\n",ptr[0], ptr[1] );
-    fprintf("Command exited with %d\n",status);              // status code other   
-    return 1;
-  }
-  else
-  {    
-      if (args.equals("fork")
+      int i;
+      printf("*** Entered:");
+      for (i = 0; args[i] != NULL; i++)
       {
-        pid_t pid, pid1;
-        pid = exec(args);
-        /* fork a child process */
-        if (pid < 0) 
-        { /* error occurred */
-          fprintf(stderr, "Fork Failed\n");
-          return 1;
-        }
-        else if (pid == 0) 
-        { /* child process */
-          pid1 = getpid();
-          printf("child: pid = %d\n",pid);
-          printf("child: pid1 = %d\n",pid1);
-        }
-        else 
-        { /* parent process */
-          pid1 = getpid();
-          printf("parent: pid = %d\n",pid);
-          printf("parent: pid1 = %d\n",pid1);
-          wait(NULL);
-        }
+        printf(" %s", args[i]);
+      }
+      printf(" (%d words)\n", i);
+    
+      int status = execvp(args[0],args);
+      if (status == 1)
+      { /* error occurred*/
+        printf("Command not found\n");
+        printf("Command exited with %d\n",status);              // status code other   
+        return;
+      }
+      else if(status == 2)
+      {
+        printf("%s: cannot access '%s' : No such file or directory exists\n",args[0],args[1] );
+        printf("Command exited with %d\n",status);              // status code other   
+        return;
       }
       else
       {
-        exec(args);
+        if (args[0] == "exit")
+          {
+              exit(0);
+          }
+        else if (status == 0 || status == -1)
+          {
+            pid_t pid, pid1;
+            pid = fork();
+            /* fork a child process*/
+            if (pid < 0) 
+            { /* error occurred*/
+              printf("Fork Failed\n");
+              printf("Command exited with %d\n",pid);              // status code other   
+            }
+            else if (pid == 0) 
+            { /* child process*/
+              pid1 = getpid();
+              printf("child: pid = %d\n",pid);
+              printf("child: pid1 = %d\n",pid1);
+            }
+            else 
+            { /* parent process*/
+              pid1 = getpid();
+              printf("parent: pid = %d\n",pid);
+              printf("parent: pid1 = %d\n",pid1);
+              wait(NULL);
+            }
+          }
+          else
+          {
+            execvp(args[0],args);
+          }
+        printf("Command exited sucessfully\n");   // status code 0
       }
-    fprintf(stderr, "Command exited sucessfully\n");   // status code 0
-  }
-
       
-
-  exit();
 }
-
